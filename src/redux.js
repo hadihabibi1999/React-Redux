27b0d77bf93d31f62a0e84a779bd2e7b6f3e3a79
HadiCyb <hadihabibi2000@gmail.com>
2019-04-19 22:48:35 +0400

    independent action function

--- src/redux.js
+++ src/redux.js
@@ -1,15 +1,25 @@
+
+
 import {createStore} from "redux";
 
-const reduce = function(state,action){
- if (action.type==='dec')
- return state+3;
- return state;
+const reduce = function(state=7,action){
+    if (action.type==="dec")
+    return state+3;
+    return state;
 }
 
-const store = createStore(reduce,2);
+const store = createStore(reduce);
+
+ function action (some) {
+     return {
+type:"dec",
+some
+     }
+ }
 
 store.subscribe(() => console.log(store.getState()));
 
-store.dispatch({type:'dec'});
+store.dispatch(action(4));
+
 
 export default store;
