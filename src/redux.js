eb57e281454fb2393fecaab7c726f1cd82aeaf26
HadiCyb <hadihabibi2000@gmail.com>
2019-04-23 17:38:18 +0400

    combineReducer

--- src/redux.js
+++ src/redux.js
@@ -4,12 +4,12 @@ import {combineReducers,createStore} from "redux";
 
 const reduce1 = function(state={},action){
     if (action.type==="INC")
-    return {...state,number:action.some+3};
+    return {...state,number:action.valu+3};
     return state;
 }
 const reduce2 = function(state={},action){
     if (action.type==="DEC")
-    return {...state,number:action.some-3};
+    return {...state,number:action.valu-3};
     return state;
 }
 const reducers = combineReducers({
@@ -18,17 +18,11 @@ const reducers = combineReducers({
 })
 const store = createStore(reducers);
 
- function action (some) {
-     return{
-    type:"DEC",
-    some
-     }
- }
 
 store.subscribe(() => console.log(store.getState()));
 
-store.dispatch(action(4));
-store.dispatch(action(5));
+store.dispatch({type:"DEC",valu:4});
+store.dispatch({type:"INC",valu:4});
 
 
 export default store;
